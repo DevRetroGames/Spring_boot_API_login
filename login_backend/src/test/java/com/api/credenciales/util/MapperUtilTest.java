14d9733f93d9a4e0725ff110389e99413b646b43
Alejandro <devretrogames@gmail.com>
2022-06-01 21:27:46 -0400

    Advance Testing: mapperUtil -> roleEntityToRoleDTO

--- login_backend/src/test/java/com/api/credenciales/util/MapperUtilTest.java
+++ login_backend/src/test/java/com/api/credenciales/util/MapperUtilTest.java
@@ -2,13 +2,14 @@ package com.api.credenciales.util;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.modelmapper.ModelMapper;
 
 import com.api.credenciales.dto.RolesDTO;
@@ -30,9 +31,11 @@ public class MapperUtilTest {
 	private RolesDTO rolDTO ;
 	
 	// -----------------------------------------------------------------
-
-	@Before
+	
+	@BeforeEach
 	public void setUp() throws Exception {
+		
+		this.mapperUtil = Mockito.mock( MapperUtil.class ) ;
 		
 		this.rol = new Roles() ;
 		this.rol.setName( "test01" ) ;
@@ -43,30 +46,30 @@ public class MapperUtilTest {
 		this.rolDTO.setStatus( true ) ;
 		
 	}
-
+	
 	// -----------------------------------------------------------------
-
+	
+	@AfterEach
+	public void tearDown() throws Exception {
+	}
+	
+	// -----------------------------------------------------------------
+	
 	@Test
 	public void testRoleEntityToRoleDTO() {
 		
+		Mockito.when( this.mapperUtil.roleEntityToRoleDTO( this.rol ) ).thenReturn( rolDTO ) ;
 		RolesDTO rolDTOTest = this.mapperUtil.roleEntityToRoleDTO( this.rol ) ;
 		
+		// name equals
+		assertEquals( this.rolDTO.getName() , rolDTOTest.getName() ) ;
+		
+		// status equals
+		assertEquals( this.rolDTO.isStatus() , rolDTOTest.isStatus() ) ;
+		
+		// dto equals
 		assertEquals( this.rolDTO , rolDTOTest ) ;
-	}
-
-	@Test
-	public void testRoleDTOToRoleEntity() {
 		
-		Roles rolTest = this.mapperUtil.roleDTOToRoleEntity( rolDTO ) ;
-		
-		assertEquals( this.rol , rolTest ) ;
-		
-	}
-	
-	// -----------------------------------------------------------------
-	
-	@After
-	public void tearDown() throws Exception {
 	}
 
 }
